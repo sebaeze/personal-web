9c576db396893da554f98be75131e34b2f4d5418
andreole <andreole@ar.ibm.com>
2019-03-05 14:00:23 -0300

    dist coom publico

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
+/*
+*
+*/
+app.use(express.static(path.join(__dirname, 'dist')));
+//
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
+/*
+*
+*/
 app.listen(process.env.PORT || 8080);
+//
