10b0ec0d6aaef6e02be9aa3f7fcea57e877b3d01
andreole <andreole@ar.ibm.com>
2019-03-05 13:02:03 -0300

    react + parcel 22

--- server.js
+++ server.js
@@ -2,27 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-/*
-* Paginas staticas
-*/
-let opciones = { dotfiles: 'ignore', etag: false, extensions: [], index: false, maxAge: '1d' , redirect: false,
-  setHeaders: function (res, path, stat) {
-      res.set('Access-Control-Allow-Origin', '*');
-      res.set('Access-Control-Allow-Methods', '*');
-  }
-} ;
-//
-app.use(express.static(path.join(__dirname, 'public' ), opciones ));
-app.use(express.static(path.join(__dirname, 'dist'   ), opciones ));
-//
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
-/*
-*
-*/
-let serverPort = process.env.PORT || 3000 ;
-app.listen( serverPort, function(){
-  console.log('Server listening on port ' + serverPort + ' localhost:'+serverPort );
-} );
-//
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
