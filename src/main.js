5e1be7a17f03ffe64f9b0a87ca2f0da330155caf
andreole <andreole@ar.ibm.com>
2019-03-11 13:15:34 -0300

    mejora en ancho de pagina

--- src/main.js
+++ src/main.js
@@ -4,14 +4,15 @@ import Header   from "./componentes/Header";
 //
 const App = () =>{
   return (
-    <div className="container-fluid">
-      <Header />
-      <div className="row">
-      </div>
-      <div className="row bg-light">
-        <div className="wp-body mt-200">
-          <br/><br/><br/><br/><br/><br/><br/>
-          <h1 className="font-roboto-mono wp-en-construccion">En Construcción</h1>
+    <div className="wrapper-body bg-light">
+      <div className="container-fluid">
+        <Header />
+        <div className="row wp-body">
+          <div className="col"></div>
+          <div className="col">
+            <span className="font-roboto-mono align-middle">En Construcción</span>
+          </div>
+          <div className="col"></div>
         </div>
       </div>
     </div>
